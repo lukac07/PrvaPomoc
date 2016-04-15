d4afdead63c676a9c891d9caedfae7cb794397d3
lukac07 <stjepan.lukac@gmail.com>
2016-04-15 21:06:45 +0200

    napravljeni tabovi s fragmentima

--- app/src/main/java/com/example/stjepanlukac/prvapomoc/UserAreaActivity.java
+++ app/src/main/java/com/example/stjepanlukac/prvapomoc/UserAreaActivity.java
@@ -2,18 +2,44 @@ package com.example.stjepanlukac.prvapomoc;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
 
 public class UserAreaActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+
+        //referenca na toolbar kad sam radio tabove
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPagerAdapter.addFragments(new PrvaPomoc(), "Prva pomoc");
+        viewPagerAdapter.addFragments(new PosaljiLokaciju(), "Posalji lokaciju");
+        viewPagerAdapter.addFragments(new TreciFragment(), "Treci fragment");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 
     @Override
